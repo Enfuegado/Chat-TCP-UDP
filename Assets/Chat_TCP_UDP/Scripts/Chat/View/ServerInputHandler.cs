094b23321a004a4d3040a98bcfbd7e01452a65d9
Enfuegado <jbarragani@academia.usbbog.edu.co>
2026-02-25 11:40:29 -0500

    Envio de imagen ahora tambien desde server

--- Assets/Chat_TCP_UDP/Scripts/Chat/View/ServerInputHandler.cs
+++ Assets/Chat_TCP_UDP/Scripts/Chat/View/ServerInputHandler.cs
@@ -1,10 +1,13 @@
 using TMPro;
 using UnityEngine;
+using System.IO;
 using System.Threading.Tasks;
 
 public class ServerInputHandler : MonoBehaviour
 {
     [SerializeField] private TMP_InputField inputField;
+
+    [SerializeField] private string imageFileName = "test.png"; //change name for other image
 
     private ChatController controller;
 
@@ -18,8 +21,33 @@ public class ServerInputHandler : MonoBehaviour
         if (controller == null)
             return;
 
-        await controller.SendTextMessage(inputField.text);
+        string message = inputField.text;
+
+        if (string.IsNullOrWhiteSpace(message))
+            return;
+
+        await controller.SendTextMessage(message);
 
         inputField.text = "";
+    }
+
+    public async void OnSendImageClicked()
+    {
+        if (controller == null)
+            return;
+
+        string path = Path.Combine(
+            Application.dataPath,
+            "Chat_TCP_UDP/StreamingAssets",
+            imageFileName
+        );
+
+        if (!File.Exists(path))
+        {
+            Debug.LogWarning($"Image not found at: {path}");
+            return;
+        }
+
+        await controller.SendImage(path);
     }
 }
